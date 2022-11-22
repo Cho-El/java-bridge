7eaa0d9fcfce1214c95491453cb55c68912d2aab
Cho-El <syzzang21c@gmail.com>
2022-11-22 23:13:04 +0900

    feat(Application): 게임 실행
    게임 실행 코드 추가

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -1,10 +1,13 @@
 package bridge;
 
+import bridge.controller.GameController;
 import org.junit.jupiter.api.Test;
 
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameController gameController = new GameController();
+        gameController.play();
     }
 }
