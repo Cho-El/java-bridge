bd4cdcf60dda0552034744d48c779fdf3b0e8be8
Cho-El <syzzang21c@gmail.com>
2022-11-22 21:52:41 +0900

    test(MoveConditionTest) : 플레이어가 이동할 값 테스트
    플레이어가 이동할 값을 유효성 검사하는 테스트 코드 추가
    
    - 변경 사항
    MoveConditionTest : 플레이어가 이동할 값을 유효성 검사하는 테스트 코드 추가

--- src/test/java/bridge/utill/MoveConditionTest.java
+++ src/test/java/bridge/utill/MoveConditionTest.java
@@ -1,14 +1,18 @@
 package bridge.utill;
 
+import bridge.domain.Error;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MoveConditionTest {
+
     @BeforeEach
     void setUp() {
         MoveCondition.initMap();
@@ -19,5 +23,16 @@ class MoveConditionTest {
     @CsvSource(value = {"1/U", "0/D"}, delimiter = '/')
     void correctMatch(int value, String move) {
         assertThat(MoveCondition.getMatchMove(value)).isEqualTo(move);
+    }
+
+    @DisplayName("플레이어가 입력한 이동할 칸이 정해진 값이 아니라면 예외가 발생한다")
+    @Test
+    void createInputWithWrongValue() {
+        String inputPosition = "A";
+        String Head = "[ERROR]";
+        assertThatThrownBy(() -> MoveCondition.validMove(inputPosition))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(Error.NOT_IN_RANGE_BRIDGE_ALPHABET.getMessage())
+                .hasMessageStartingWith(Head);
     }
 }
